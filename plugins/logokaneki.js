72c4ea10d7f7d0c0221e7512d67432795de89ccc
Tio Permana <94074076+BOTCAHX@users.noreply.github.com>
2022-02-06 22:50:31 +0700

    Create logokaneki.js

---
+++ plugins/logokaneki.js
@@ -0,0 +1,16 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn, args }) => {
+   response = args.join(' ').split('|')
+  if (!args[0]) throw 'Masukkan Parameter'
+  m.reply('Proses...')
+  let res = `https://bx-hunter.herokuapp.com/api/canvas/gfx1?apikey=FuckBitch&nama=${response[0]}`
+  conn.sendFile(m.chat, res, 'kaneki.jpg', `Sudah Jadi`, m, false)
+}
+handler.help = ['kaneki'].map(v => v + ' <text>')
+handler.tags = ['nulis']
+handler.command = /^(logokaneki)$/i
+handler.register = true
+
+handler.limit = true
+
+module.exports = handler
