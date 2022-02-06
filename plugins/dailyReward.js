b4de933ab3d574096a7a91ae775a98dc4241488b
Tio Permana <94074076+BOTCAHX@users.noreply.github.com>
2022-02-06 18:05:39 +0700

    fix

--- plugins/dailyReward.js
+++ plugins/dailyReward.js
@@ -3,7 +3,7 @@ let handler = async(m, { conn, usedPrefix, args, command }) => {
     let name = conn.getName(m.sender)
     let user = db.data.users[m.sender]
     if (user.dailyReward < 20){
-        await conn.sendButtonLoc(m.chat, await(await fetch(thumbfoto)).buffer(), `Daily Reward kamu belum mencukupi untuk mendapatkan hadiah Daily Reward!\nDaily Reward mu: ${user.dailyReward}/20. Dibutuhkan 20 Daily Reward untuk mendapatkan hadiah.\nCara memperoleh Daily Reward: bermain semua game di Haruno Bot kecuali Family100, Tictactoe, Slot, Suit. Untuk Math hanya Math level hard keatas untuk dapat memperoleh hadiah.\n\nPoint Daily Reward akan di riset setiap jam 4 pagi.`, watermark, 'Game menu', '.? game')
+        await conn.sendButtonLoc(m.chat, await(await fetch(thumbfoto)).buffer(), `Daily Reward kamu belum mencukupi untuk mendapatkan hadiah Daily Reward!\nDaily Reward mu: ${user.dailyReward}/20. Dibutuhkan 20 Daily Reward untuk mendapatkan hadiah.\nCara memperoleh Daily Reward: bermain semua game di R-Txyz kecuali Family100, Tictactoe, Slot, Suit. Untuk Math hanya Math level hard keatas untuk dapat memperoleh hadiah.\n\nPoint Daily Reward akan di riset setiap jam 4 pagi.`, watermark, 'Game menu', '.? game')
     }
     if (user.dailyReward === 10) {
         await conn.sendButtonLoc(m.chat, await(await fetch(thumbfoto)).buffer(),`Terimakasih sudah menyelesaikan daily reward. Berikut hadiah mu:
