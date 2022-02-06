8f2421de31220957a198b2b3ce53a867873d8234
AlyaaXzy <96462315+AlyaaXd@users.noreply.github.com>
2022-02-06 09:04:05 +0700

    Create viagopay.js

---
+++ plugins/viagopay.js
@@ -0,0 +1,34 @@
+let handler  = async (m, { conn, usedPrefix: _p }) => {
+let info = `╠═〘 PEMBAYARAN 〙 ═
+╠═ Untuk Pembayaran Via GOPAY
+╠➥ *085730903853*
+║- Chat Owner Terlebebih
+║ Dahulu
+╠═〘 Fitur 〙 ═
+║
+╠➥ *FITUR WELCOME*
+║    *FITUR STIKER*
+╠➥ *FITUR ADMIN*
+║    *KICK ORANG*
+║    *JADIIN ADMIN*
+╠➥ *FITUR ISLAM*
+║   *QURAN*
+║   *CEK JADWAL SHALAT*
+╠➥ *INTERNET*
+║   *BRAINLY*
+║   *GOOGLE*
+╠➥ *DAN 200 LEBIH*
+║   *FITUR LAINNYA*
+║ 
+╠═ ©2021 wabot-aq
+╠═ Scrip original by Nurutomo
+╠═〘 R-Txzy 〙 ═
+`.trim()
+
+conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '*Sewa Bot*', 'status@broadcast') 
+}
+handler.help = ['viadana']
+handler.tags = ['info']
+handler.command = /^viadana$/i
+
+module.exports = handler
