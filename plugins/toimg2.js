58dcf3f405d0089e599ea0ab0f110de323897d2a
AlyaaXzy <96462315+AlyaaXd@users.noreply.github.com>
2022-02-06 23:22:47 +0700

    Update toimg2.js

--- plugins/toimg2.js
+++ plugins/toimg2.js
@@ -1,3 +1,19 @@
+let fs = require('fs')	
+const {
+    MessageType,
+    Mimetype
+} = require("@adiwajshing/baileys");
+const anu = {
+	key : {
+           participant : '0@s.whatsapp.net'
+                        },
+       message: {
+                    documentMessage: {
+                    title: 'Hmm.jpeg', 
+                    jpegThumbnail: fs.readFileSync('./src/R-Txzy.jpg')
+                          }
+                        }
+                      }
 let { webp2png } = require('../lib/webp2mp4')
 let handler = async (m, { conn, usedPrefix, command }) => {
   if (!m.quoted) throw `balas stiker dengan perintah *${usedPrefix + command}*`
@@ -8,9 +24,9 @@ let handler = async (m, { conn, usedPrefix, command }) => {
   if (/webp/.test(mime)) {
     out = await webp2png(media)
   }
-  await conn.sendFile(m.chat, out, 'out.png', watermark, m, false, { thumbnail: out })
+  await conn.sendFile(m.chat, out, 'out.png', '© R-Txzy', anu, false, { thumbnail: fs.readFileSync('./src/R-Txzy.png')})
 }
-handler.help = ['toimg2']
+handler.help = ['toimg']
 handler.tags = ['sticker']
-handler.command = ['toimg2']
+handler.command = ['toimg']
 module.exports = handler
