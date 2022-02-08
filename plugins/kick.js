4acb13800565664454a13966d5e6d89ba81f444c
Tio Permana <94074076+BOTCAHX@users.noreply.github.com>
2022-02-08 12:12:35 +0700

    Update kick.js

--- plugins/kick.js
+++ plugins/kick.js
@@ -1,14 +1,22 @@
+// Coba dlu kalo error fix sendiri
+
 let handler = async (m, { conn, args }) => {
   let ownerGroup = m.chat.split`-`[0] + '@s.whatsapp.net'
   let users = m.mentionedJid.filter(u => !(u == ownerGroup || u.includes(conn.user.jid)))
   for (let user of users) if (user.endsWith('@s.whatsapp.net')) await conn.groupRemove(m.chat, [user])
 }
-handler.help = ['kick >Error', '-'].map(v => v + ' @user')
-handler.tags = ['admin']
+handler.help = ['kick','-'].map(v => 'o' + v + ' @user')
+handler.tags = ['owner']
 handler.command = /^(kick|\-)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
 handler.group = true
+handler.private = false
+
 handler.admin = true
 handler.botAdmin = true
-handler.limit = true
-module.exports = handler
 
+handler.fail = null
+
+module.exports = handler
