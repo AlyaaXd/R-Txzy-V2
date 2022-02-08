7110359b4872e824867af39db94d5ab74f2d3c50
Tio Permana <94074076+BOTCAHX@users.noreply.github.com>
2022-02-08 11:53:21 +0700

    button

--- plugins/botstat.js
+++ plugins/botstat.js
@@ -1,26 +1,30 @@
+// Update button v2
+// BOTCAHX 
+//update dikit:v
+
 let handler = async (m, { conn }) => {
-    let { anon, anticall, antispam, antitroli, backup, jadibot, groupOnly, nsfw, statusupdate, autogetmsg, antivirus, publicjoin } = global.db.data.settings[conn.user.jid]
+    let { anon, anticall, antispam, antitroli, backup, jadibot, groupOnly, nsfw } = global.db.data.settings[conn.user.jid]
     const chats = conn.chats.all()
     const groups = chats.filter(v => v.jid.endsWith('g.us'))
     let totaljadibot = [...new Set([...global.conns.filter(conn => conn.user && conn.state !== 'close').map(conn => conn.user)])]
 
+    let wm = global.botwm
     let _uptime = process.uptime() * 1000
     let uptime = clockString(_uptime)
 
-    m.reply(`
-┌─〔 Status 〕
-├ Aktif selama ${uptime}
-├ Baterai ${conn.battery != undefined ? `${conn.battery.value}% ${conn.battery.live ? '🔌 pengisian' : ''}` : 'tidak diketahui'}
-├ *${groups.length}* Grup
-├ *${chats.length - groups.length}* Chat Pribadi
-├ *${Object.keys(global.db.data.users).length}* Pengguna
-├ *${totaljadibot.length}* Jadibot
-├ *${conn.blocklist.length}* Terblock
-├ *${Object.entries(global.db.data.chats).filter(chat => chat[1].isBanned).length}* Chat Terbanned
-├ *${Object.entries(global.db.data.users).filter(user => user[1].banned).length}* Pengguna Terbanned
-└────
-
-┌─〔 Pengaturan 〕
+    let str = `
+╭─────[ *Status* ]────✧
+├◌ Aktif selama ${uptime}
+├◌ Baterai ${conn.battery != undefined ? `${conn.battery.value}% ${conn.battery.live ? '🔌 pengisian' : ''}` : 'tidak diketahui'}
+├◌ *${groups.length}* Grup
+├◌ *${chats.length - groups.length}* Chat Pribadi
+├◌ *${Object.keys(global.db.data.users).length}* Pengguna
+├◌ *${totaljadibot.length}* Jadibot
+├◌ *${conn.blocklist.length}* Terblock
+├◌ *${Object.entries(global.db.data.chats).filter(chat => chat[1].isBanned).length}* Chat Terbanned
+├◌ *${Object.entries(global.db.data.users).filter(user => user[1].banned).length}* Pengguna Terbanned
+╰────────────···
+╭─────[ *Pengaturan* ]────✧
 ├ ${anon ? '✅' : '❌'} *Anon Chat*
 ├ ${anticall ? '✅' : '❌'} *Anti Call*
 ├ ${antispam ? '✅' : '❌'} *Anti Spam*
@@ -29,8 +33,10 @@ let handler = async (m, { conn }) => {
 ├ ${groupOnly ? '✅' : '❌'} *Mode Grup*
 ├ ${jadibot ? '✅' : '❌'} *Jadi Bot*
 ├ ${nsfw ? '✅' : '❌'} *Mode Nsfw*
-└────
-    `.trim())
+╰────────────···
+    `.trim()
+conn.send2Button(m.chat, str, wm, 'Info', '.speed', 'Owner', '.owner',m)
+conn.reply(str)
 }
 handler.help = ['botstatus']
 handler.tags = ['info']
