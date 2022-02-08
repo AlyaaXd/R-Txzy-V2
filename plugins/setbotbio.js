318ddc3c799a836464fa69ac16f5acb597f587e0
Tio Permana <94074076+BOTCAHX@users.noreply.github.com>
2022-02-08 22:37:07 +0700

    Update setbotbio.js

--- plugins/setbotbio.js
+++ plugins/setbotbio.js
@@ -1,6 +1,6 @@
 // NurNurz
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-  if (!text) throw `Harap masukan text!\n\nCotoh: ${usedPrefix + command} Haruno Bot`
+  if (!text) throw `Harap masukan text!\n\nCotoh: ${usedPrefix + command} BOTCAHX Bot`
   try {
     await conn.setStatus(text)
     m.reply('Berhasil!')
