f35ba69970a19efa1b19229ed2ebaff6af592624
AlyaaXzy <96462315+AlyaaXd@users.noreply.github.com>
2022-02-09 21:54:39 +0700

    Update promote.js

--- plugins/promote.js
+++ plugins/promote.js
@@ -3,14 +3,15 @@ let handler = async (m, { conn, participants }) => {
   let users = m.mentionedJid.filter(user => members.includes(user))
   for (let user of users) await conn.groupMakeAdmin(m.chat, [user]).catch(console.log)
 }
-handler.help = ['promote','admin','^', '↑'].map(v => v + ' @user')
-handler.tags = ['admin']
+handler.help = ['promote', 'admin', '^'].map(v => 'o' + v + ' @user')
+handler.tags = ['owner']
 
-handler.command = /^(promote|admin|\^|↑)$/i
+handler.command = /^(promote|admin|o\^)$/i
 
+handler.owner = true
 handler.group = true
 
-handler.admin = true
+handler.admin = false
 handler.botAdmin = true
 
 module.exports = handler
