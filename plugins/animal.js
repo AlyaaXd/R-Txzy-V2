9d70d16e6291ed12d4429a9a67ea4a894e279a5f
Tio Permana <94074076+BOTCAHX@users.noreply.github.com>
2022-02-10 13:24:03 +0700

    Update animal.js

--- plugins/animal.js
+++ plugins/animal.js
@@ -17,7 +17,7 @@ ${usedPrefix}${command} panda
   if (!res.ok) throw `${res.status} ${res.statusText}`
   let json = await res.json()
   if (!json.image) throw json
-  conn.sendFile(m.chat, json.image, '', `${json.fact}\n\n© R-Txyz`, m)
+  conn.sendFile(m.chat, json.image, '', `${json.fact}\n\n© Animal`, m)
 }
 handler.help = ['animal'].map((v) => v + ' <opsi>')
 handler.tags = ['internet']
