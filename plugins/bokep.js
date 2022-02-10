e4e807cbe3a637c75bd8e4c5117dddde3005bea3
Tio Permana <94074076+BOTCAHX@users.noreply.github.com>
2022-02-10 13:24:57 +0700

    Update bokep.js

--- plugins/bokep.js
+++ plugins/bokep.js
@@ -8,7 +8,7 @@ handler.help = ['bkp']
 handler.tags = ['bokep']
 
 handler.command = /^(bkp)$/i
-handler.premium = false
+handler.premium = true
 handler.register = true
-handler.limit = 30
+handler.limit = 1
 module.exports = handler
