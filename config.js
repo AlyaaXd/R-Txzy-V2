d00eb2e036e617f965ddded2708d6192d3b56400
wannjb <113450108+wannjb@users.noreply.github.com>
2022-10-06 19:55:34 +0800

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 
 
 let fs = require('fs')
-global.owner = ['6285730903853', '6282221792667','6285730903853', '6282221792667', '6282135905896'] // Letakan nomor kamu disini
+global.owner = ['62895327999292', '62895327999292','62895327999292', '62895327999292', '62895327999292'] // Letakan nomor kamu disini
 global.APIs = { // API Prefix
   // nama: 'https://website'
   hardianto: 'https://hardianto-chan.herokuapp.com',
@@ -41,10 +41,10 @@ global.APIKeys = { // APIKey nya disini
 }
 
 // Sticker WM
-global.packname = 'Exc'
-global.author = 'NyyXz'
+global.packname = 'WannTrue BOT'
+global.author = '© WannTrue'
 
-global.wait = '「 ⏱️ 」Harap tunggu...'
+global.wait = '「 ⏱️ 」Mohon menunggu ya anak *ANJ*'
 global.eror = '「❗」Server ERROR!'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
 global.watermark = '© R-Txzy' //change the watermark 
